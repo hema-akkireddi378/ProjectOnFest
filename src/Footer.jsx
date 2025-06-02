0c33f8ca2bdb4193ef2030265e086b09c272705d
Akkireddi Hema <akkireddihema@gmail.com>
2025-06-02 09:49:21 +0530

    faculty dashboard update

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -70,23 +70,73 @@ export function FacultyDashboard() {
              </div>
              </div>
              <div className='assignTheCoordinators'>
-                <p>Assign the Event Coordinators</p>
-                <select>
-                    <option value="none">None</option>
-                    <option value="Banner Launch">Banner Launch</option>
-                    <option value="Traditional Day">Traditional Day</option>
-                    <option value="Final Fest">Final Fest</option>
-                    <option value="Men's Cricket">Men's Cricket</option>
-                    <option value="Men's Kabaddi">Men's Kabaddi</option>
-                    <option value="Men's Volley Ball">Men's Volley Ball</option>
-                    <option value="Men's Kho Kho">Men's Kho Kho</option>
-                    <option value="Carroms">Carroms</option>
-                    <option value=" Pubg">Pubg</option>
-                    <option value="Free Fire">Free Fire</option>
-                    <option value="Women Throw Ball">Women Throw Ball</option>
+                <p className='assignCoordinatorHeader'>Assign the Event Coordinators</p>
+                <select className='SelectOptions'>
+                    <option value="none" className='options'>None</option>
+                    <option value="Banner Launch" className='options'>Banner Launch</option>
+                    <option value="Traditional Day" className='options'>Traditional Day</option>
+                    <option value="Final Fest" className='options'>Final Fest</option>
+                    <option value="Men's Cricket" className='options'>Men's Cricket</option>
+                    <option value="Men's Kabaddi" className='options'>Men's Kabaddi</option>
+                    <option value="Men's Volley Ball" className='options'>Men's Volley Ball</option>
+                    <option value="Men's Kho Kho" className='options'>Men's Kho Kho</option>
+                    <option value="Carroms" className='options'>Carroms</option>
+                    <option value=" Pubg" className='options'>Pubg</option>
+                    <option value="Free Fire" className='options'>Free Fire</option>
+                    <option value="Women Throw Ball" className='options'>Women Throw Ball</option>
                 </select>
-
+              
              </div>
+             <div className='coordinators'>
+                <input type='text' placeholder='Name' className='coordinatorDetails'></input>
+                <input type='text' placeholder='Branch' className='coordinatorDetails'></input>
+                <input type='text' placeholder='Year' className='coordinatorDetails'></input>
+                <input type='text' placeholder='Department' className='coordinatorDetails'></input>
+                <input type='text' placeholder='Section' className='coordinatorDetails'></input>
+
+                  {/* <p className='coordinatorDetails'>Coordinator Name</p>
+                <p className='coordinatorDetails'>Brach</p>
+                <p className='coordinatorDetails'>Year</p>
+                <p className='coordinatorDetails'>Department</p>
+                <p className='coordinatorDetails'>Section</p> */}
+             </div>
+              <p className='setCoordinator'>Set As A Coordinator</p>
+
+
+              <div className='assignTheCoordinators'>
+                <p className='assignEventHeader'>Assign the Events</p>
+                <select className='SelectOptions'>
+                    <option value="none" className='options'>None</option>
+                    <option value="Banner Launch" className='options'>Banner Launch</option>
+                    <option value="Traditional Day" className='options'>Traditional Day</option>
+                    <option value="Final Fest" className='options'>Final Fest</option>
+                    <option value="Men's Cricket" className='options'>Men's Cricket</option>
+                    <option value="Men's Kabaddi" className='options'>Men's Kabaddi</option>
+                    <option value="Men's Volley Ball" className='options'>Men's Volley Ball</option>
+                    <option value="Men's Kho Kho" className='options'>Men's Kho Kho</option>
+                    <option value="Carroms" className='options'>Carroms</option>
+                    <option value=" Pubg" className='options'>Pubg</option>
+                    <option value="Free Fire" className='options'>Free Fire</option>
+                    <option value="Women Throw Ball" className='options'>Women Throw Ball</option>
+                </select>
+              
+             </div>
+             <div className='coordinators'>
+                <input type='text' placeholder='Event Date (DD-MM-YYYY)' className='coordinatorDetails'></input>
+                <input type='text' placeholder='Event Start Time ex 9am' className='coordinatorDetails'></input>
+                <input type='text' placeholder='Event End Time ex 5pm' className='coordinatorDetails'></input>
+                <input type='text' placeholder='Section' className='coordinatorDetails'></input>
+
+                  {/* <p className='coordinatorDetails'>Coordinator Name</p>
+                <p className='coordinatorDetails'>Brach</p>
+                <p className='coordinatorDetails'>Year</p>
+                <p className='coordinatorDetails'>Department</p>
+                <p className='coordinatorDetails'>Section</p> */}
+             </div>
+              <p className='setEvent'>Set As A Event</p>
+              <p className='hoddetails'>Only access to ,<br></br>
+              Head Of the Department, CSE HOD<br></br>
+              Mr Raja Sheaker</p>
         </div>
     )
 }
